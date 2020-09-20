25ef4f83b62e5b181d0658cf1371b3dd97743b16
Shakana Fox <foxshakana@gmail.com>
2020-09-20 16:11:49 -0400

    added functionsor uppercase letters

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var generatePassword = prompt("How many characters would you like your password to contain?"){
+  alert("Click OK to confirm including special characters.");
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+function getRandomLower(){
+  return (Math.floor(Math.random() * 128));
+}
 
+function getRandomUpper(){
+  return (Math.floor(Math.random() * 128));
+}
+
+function getRandomSpecial(){
+  return (symbols);
 }
 
 //need prompt
@@ -17,21 +32,26 @@ function writePassword() {
 //   alert(Click OK to confirm including lowercase characters);...name getRandomLower
 //   alert(Click OK to confirm including uppercase characters.);...name getRandomUpper  
 //     }
-
+//https://net-comber.com/charset.html character sets
 
 // Generate functions (https://owasp.org/www-community/password-special-characters) 
 //   function getRandomLower(){
 //   return (Math.floor(Math.random()* 128));
-//  console.log(getRandomLower());
-}
 
-function generatePassword(//password ){
-   return "This will be replaced by my generated password.";
-   // if (password > 8) {
-   //  
+//   function getRandomUpper(){
+//   return (Math.floor(Math.random()* 128));
+
+//  console.log(getRandomLower());
+
+//  function getRandomSpecial(){
+ //   return symbols[0];
+
+
+
+
+function generatePassword(){
+   return ("This will be replaced by my generated password."); 
    }
-}
-//if () 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
